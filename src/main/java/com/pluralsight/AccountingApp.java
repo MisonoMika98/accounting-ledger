3c8e7284bf50cfe97bf102a0e65691674b8225e4
ariel raymundo <ariraymundo98@gmail.com>
2026-04-27 11:52:26 -0700

    added ledger display screen

--- src/main/java/com/pluralsight/AccountingApp.java
+++ src/main/java/com/pluralsight/AccountingApp.java
@@ -45,7 +45,7 @@ public class AccountingApp
                 break;
 
             case "L":
-                System.out.println("ledger display screen placeholder");
+                displayLedgerScreen();
                 break;
 
             case "X":
@@ -60,10 +60,67 @@ public class AccountingApp
     }
 
 
+
+    static void displayLedgerScreen()
+    {
+        System.out.println();
+        System.out.println("Ledgers Display");
+        System.out.println("---------------------------------");
+        System.out.println("A) Display All Entries");
+        System.out.println("D) Deposits");
+        System.out.println("P) Payments");
+        System.out.println("R) Reports");
+        System.out.println("H) Back To Home Screen");
+        System.out.println();
+        System.out.print("Make a choice: ");
+        String choice2  = userInput.nextLine().toUpperCase().strip();
+
+        System.out.println();
+
+        switch (choice2)
+        {
+            case "A":
+                System.out.println("display all entries placeholder");
+                break;
+
+            case "D":
+                System.out.println("display entries that are deposits into account placeholder");
+                break;
+
+            case "P":
+                System.out.println("display only negative payments/entries placeholder");
+                break;
+
+            case "R":
+                System.out.println("Reports screen placeholder");
+                break;
+
+            case "H":
+                displayHomeScreen();
+
+            default:
+                System.out.println("Error, please try again");
+                displayLedgerScreen();
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // WORK IN PROGRESS
     // methods to create and log user transactions in a .csv file (W.I.P)
     private static boolean writeHeader = false;
 
-    private static void logTransactions(String action, String message) {
+    private static void logTransactions(String userDateInput, String action, String message, double amount) {
 
         FileOutputStream fileOutputStream = null;
         PrintWriter printWriter = null;
@@ -74,9 +131,9 @@ public class AccountingApp
             printWriter = new PrintWriter(fileOutputStream);
 
             // gets the specific time for the transaction logs
-            LocalDateTime now = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String timeStamp = now.format(formatter);
+//            LocalDateTime now = LocalDateTime.now();
+//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+//            String timeStamp = now.format(formatter);
 
             // header for the .csv file, only prints ONCE thanks to the boolean,
             // but will print again if the app is restarted
@@ -86,8 +143,8 @@ public class AccountingApp
                 writeHeader = true;
             }
 
-            // logs into the .csv file
-            printWriter.println(timeStamp + " " + action + " " + message);
+            // logs into the .csv file (insert formatter)
+            printWriter.println(userDateInput + "|" + "|" + action + "|" + message + "|" + amount);
             printWriter.printf("%-15s %-10s %-25s %-15s%n", "|", "|", "|", "|");
         }
 
